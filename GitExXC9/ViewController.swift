c950b0d6d881842fb2e04412c5ea43849a4fab55
Nick Woetko <nick.woetko@gmail.com>
2018-05-06 04:42:54 -0400

    Initial Commit

---
+++ GitExXC9/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitExXC9
+//
+//  Created by Nickalos Woetko on 5/6/18.
+//  Copyright © 2018 Nick Woetko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
